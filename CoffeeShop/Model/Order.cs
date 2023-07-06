ec503ec006f2d4aff68b840fc59f3c7b2c31d5fb
RafiWick <rafiwick@gmail.com>
2023-07-06 14:12:10 -0600

    Order Class Done

--- CoffeeShop/Model/Order.cs
+++ CoffeeShop/Model/Order.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace CoffeeShop.Model
 {
-    internal class Order
+    public class Order
     {
+        public int Id { get; set; }
+        public List<Item> Items { get; set; }
+
+        public Order()
+        {
+            Items = new List<Item>();
+        }
+
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public double Total()
+        {
+            return Items.Select(item => item.PriceInDollars()).Sum();
+        }
     }
 }
